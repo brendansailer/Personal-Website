3675195425a27964223e9d22f9017992dcc2d18f
Brendan Sailer <root@DESKTOP-5958L5V.localdomain>
2021-02-07 15:03:15 -0500

    Update footer links and change order

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import Link from "next/link";
 
 export default function Footer() {
     return (
         <div className="flex justify-center py-4 px-8 bg-gray-300 text-white">
             <div className="inline-block ml-8">
-                <img src="/github.png" width="25" height="25"></img>
+                <Link href="https://www.linkedin.com/in/brendan-sailer"><img src="/linkedin.svg" width="25" height="25"></img></Link>
             </div>
             <div className="inline-block ml-8">
-                <img src="/linkedin.svg" width="25" height="25"></img>
+                <Link href="https://www.github.com/brendansailer"><img src="/github.png" width="25" height="25"></img></Link>
             </div>
         </div>
     )
